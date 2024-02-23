ac7f5cc50cfe1452636d7b1e11372972eaa99beb
Abdallah <abdollheid22@gmail.com>
2024-02-23 16:00:25 +0200

    changes

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,15 +1,33 @@
 import { useState } from "react";
 
 export default function TodoApp(){
+    const [todoList, setTodoList] = useState([]);
     const [todo, setTodo] = useState('');
+
+    const onClickHandler = () => {
+        // todoList.push(todo);
+        setTodoList((prevState) => {
+            return [...prevState, todo]
+        });};
+
+    const onTextChangeHandler = (event) => {
+        setTodo(event.target.value);
+    };
+    
     return <>
-        <input type="text" onChange={ (event) => {
-            setTodo(event.target.value);
-        }}/>
-      
-        <h1>{todo}</h1>
+        <input type="text" onChange={onTextChangeHandler}/>
+
+        <button onClick= {onClickHandler}> Add todo</button>
+        
+        {todoList.map((element) => { return <h3>{element}</h3>})}
+    
     </>; 
 }
+
+
+// posts = ['task1', 'task2', 'task3'];
+// posts = ['task1task1', 'task2task2', 'task3task3'];
+// posts = [<h1>'task1'</h1>, <h1>'task2'</h1>, <h1>'task3'</h1>];
 // challenge create new component TodoApp2 
 // has input field one row of todo
 
